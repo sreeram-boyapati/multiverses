5883a534468e66652a1ce52203113acbe907742f
Sreeram Boyapati <sreeram.boyapati2011@gmail.com>
2018-08-20 17:09:41 +0530

    [Index] clear bug in task2 and update balance by adding new persons

--- index.js
+++ index.js
@@ -2,10 +2,46 @@ const express = require("express")
 const bodyParser = require('body-parser')
 
 const app = express()
-const average = arr => arr.reduce( ( p, c ) => p + c, 0 ) / arr.length;
 
 app.use(bodyParser.json())
 models = require("./models")
+
+/**
+ * Takes the families with a particular id from all the universes
+ * and balances the power to same level by adding more persons
+ *
+ */
+const balanceFamilies = families => {
+    // Map the families
+    power_tuples = families.map(family => family.dataValues.power_count);
+
+    var maxPower = 0;
+    for (var index=0; index<power_tuples.length; index++) {
+        if (power_tuples[index] > maxPower){
+            maxPower = power_tuples[index];
+        }
+    }
+
+    for (var index=0; index<families.length; index++)
+    {
+        addPowerToFamily(families[index], maxPower);
+    }
+}
+
+function addPowerToFamily(family, maxPower) {
+    power_count = family.dataValues.power_count;
+    diffPower = maxPower - power_count;
+
+    if (diffPower > 0) {
+        for (var i=0; i<diffPower; i++){
+            models.Person.create({
+                'universe_id': family.dataValues.universe_id,
+                'power': true,
+                'family_id': family.dataValues.family_id
+            });
+        }
+    }
+}
 
 app.get("/", function(req, res) {
     res.setHeader('Content-Type', 'application/json');
@@ -51,38 +87,30 @@ app.get("/family/:id/power", function(req, res) {
             family_id: id
         }
     }).then(function(persons) {
-        var powers = [];
-        for (person in persons) {
-            power_count = persons[person].dataValues.power_count;
-            powers.push(power_count);
-        }
         var samePower = true;
         var power_min = persons[0].dataValues.power_count;
-        for (power in powers) {
-            if (power !== power_min) {
+        for (var i=0; i<persons.length; i++) {
+            var cur_power = persons[i].dataValues.power_count;
+            if (cur_power !== power_min) {
                 samePower = false;
             }
         }
-        res.json({'PowerLevelSame': samePower, 'family_id': id, 'success': true});
+        res.json({'PowerLevelSame': samePower, 'families': persons, 'success': true});
     })
 })
 
-app.get("/family/:id/balance", function(req, res) {
-    var id = req.params.id
+app.post("/family/:id/balance", function(req, res) {
+    var familyId = req.params.id
     models.Person.findAll({
         attributes: ['family_id', 'universe_id', [models.sequelize.fn('count', models.sequelize.col('power')), 'power_count']],
         group: ['universe_id'],
         where: {
-            family_id: id,
+            family_id: familyId,
         }
     }).then(function(persons) {
-        var sum_powers = 0;
-        for (person in persons) {
-            sum_powers = sum_powers + persons[person].dataValues.power_count;
-        }
-        var avg_power = sum_powers/persons.length;
-        res.json({'families': persons, 'avg_power': avg_power});
-    })
+        balanceFamilies(persons);
+        res.json({'success': true});
+    });
 });
 
 app.listen(3000)
